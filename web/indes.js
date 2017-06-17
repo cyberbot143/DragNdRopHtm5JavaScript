e8b4d58ec7487cb06dfadea837de4f10fc8f5707
S525777 <krishnasai952@gmail.com>
2017-06-17 13:18:35 -0400

    As per Client Requiremenent
    when you drag a image and drop in a container and duplicate images not allowed in container

--- web/indes.js
+++ web/indes.js
@@ -3,10 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-
-
 var drag_images = document.getElementsByClassName('drag_img');
 var drop_cntr = document.getElementById('drop_here');
 var img_len = drag_images.length;
@@ -21,36 +17,34 @@ for (var i = 0; i < img_len; i++) {
 drop_cntr.addEventListener('dragover', function (event) {
     event.preventDefault();
     return false;
-
 });
 drop_cntr.addEventListener('drop', function (event) {
-    var remove_src = '<img src ="http://www.clker.com/cliparts/e/0/f/4/12428125621652493290X_mark_18x18_02.svg.med.png" style = "width:10px" id ="remove_id" >';
+  
     var cntrId = event.dataTransfer.getData('cntrId');
+      var remove_src = '<img src ="http://www.clker.com/cliparts/e/0/f/4/12428125621652493290X_mark_18x18_02.svg.med.png" style = "width:10px" id ="remove_id' + cntrId+'" >';
     imgSrc = document.getElementById(cntrId + '_img').getAttribute('src');
     caption = document.getElementById(cntrId + '_txt').innerHTML,
     html = '<div id = "' + cntrId + 'Container" ><img src=" ' + imgSrc + ' " style = "width:50px;"  />';
     html += "<label>" + caption + " </label>";
+    //append X mark image to the new div id for image & caption
     html += remove_src + "</div>";
+    
     //checking whether the container has duplicate id
     if (document.getElementById(cntrId + 'Container') === null) {
-        //if no any unique image insert into it
-        document.getElementById('drop_here').insertAdjacentHTML('afterbegin', html);
+     //if no any unique image insert into it
+  document.getElementById('drop_here').insertAdjacentHTML('afterbegin', html);
+  document.getElementById('remove_id'+cntrId).addEventListener('click', function () {
+        var elementToBeRemove = document.getElementById('remove_id'+cntrId);
+         elementToBeRemove.parentNode.remove();
+    });
+    
     } else {
         alert('Image alreadyExists');
-    }
-    document.getElementById('remove_id').addEventListener('click', function () {
-
-        var elementToBeRemove = document.getElementById('remove_id');
-        elementToBeRemove.parentNode.remove(elementToBeRemove);
-
-        //var childDiv = document.getElementById('removeId');
-
-
-
-    });
-
-
+        //remove element onclick
+  }
+  
 });
+
 
 
 
